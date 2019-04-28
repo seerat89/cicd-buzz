349d9201218ebe12aca83738f695eddfc685c642
Ahmad Seerat Amini <srt.amini@gmail.com>
2019-04-28 13:52:18 +0300

    Conf Modified

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,7 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG
+$DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
+docker build -f Dockerfile -t $DOCKER_PATH .
+docker push $DOCKER_PATH
+#docker push $TRAVIS_REPO_SLUG
