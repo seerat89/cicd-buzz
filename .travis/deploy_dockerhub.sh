4d7041195a6e38138d0f4119c7ecad9eb0d09e68
Ahmad Seerat Amini <srt.amini@gmail.com>
2019-04-28 17:56:54 +0300

    Change the TAG var in DOCKER_PATH

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
+
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-$DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
+DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO
 docker build -f Dockerfile -t $DOCKER_PATH .
-docker push $DOCKER_PATH
-#docker push $TRAVIS_REPO_SLUG
+docker push $DOCKER_PATH:TAG
